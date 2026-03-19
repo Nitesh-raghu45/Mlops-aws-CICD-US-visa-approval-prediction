323193b418fa5630a890f2a0e81b87113156f473
Nitesh-raghu45 <niteshraghuwanshi68@gmail.com>
2026-03-19 12:54:55 +0530

    here i have completed the Explonatory DATA Analysis

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="Nitesh Raghuwanshi",
+    author_email="niteshraghuwanshi68@gmail.com",
+    packages=find_packages()
+)
